8c16407b68a5335eef90c1202c59035643e0c58d
Nandeesh Rajashekar <nandeesh.rajashekar@citrix.com>
2014-06-18 11:51:27 -0700

    First Commit

---
+++ TestProject/src/com/example/mainproject/test/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.mainproject.test;
+
+
+import org.junit.Test;
+
+import com.example.mainproject.MainActivity;
+
+
+public class MainActivityTest{
+
+	@Test
+	public void testFirst() {
+		MainActivity manActivity = null;
+	}
+}
